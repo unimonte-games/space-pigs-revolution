8e9627f3c7b9ea242768436415ab5b903da97b2a
Andre-LA <andrluiz.alvares.alvares84@gmail.com>
2019-07-11 20:42:02 -0300

    correção: inicialização das caixas de seleção configuradoras de som
    Eram sempre ativas, o problema é que, se desativar, entrar no jogo,
    sair, e então abrir a configuração novamente, aparecerá erroneamente
    como ativada.

--- Assets/Codigos/SomConf.cs
+++ Assets/Codigos/SomConf.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SomConf : MonoBehaviour
 {
     public bool isEfeitoSonoro;
 
+    public Toggle[] configuradores;
+
+    bool ObterConf()
+    {
+        return isEfeitoSonoro
+            ? GerenciadorJogo.efsonsMudo
+            : GerenciadorJogo.musicaMudo;
+    }
+
     void Start()
     {
+        bool conf = ObterConf();
+        for (int i = 0; i < configuradores.Length; i++)
+            {configuradores[i].isOn = conf; print("isOn" + conf.ToString());}
+
         AtualizaLocal();
     }
 
@@ -15,17 +29,11 @@ public class SomConf : MonoBehaviour
     {
         var todos = FindObjectsOfType<SomConf>();
         for (int i = 0; i < todos.Length; i++)
-        {
             todos[i].AtualizaLocal();
-        }
     }
 
     void AtualizaLocal()
     {
-        bool conf = isEfeitoSonoro
-            ? GerenciadorJogo.efsonsMudo
-            : GerenciadorJogo.musicaMudo;
-
-        GetComponent<AudioSource>().enabled = conf;
+        GetComponent<AudioSource>().enabled = ObterConf();
     }
 }
